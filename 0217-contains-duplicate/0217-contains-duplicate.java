0ebc645f03a1a20be381d9664557268f737a9c64
Samyak Shreyash <sam.shreyash@gmail.com>
2024-01-01 16:46:47 +0530

    Time: 18 ms (32.9%), Space: 61.8 MB (5.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,11 +3,9 @@ class Solution {
         Set<Integer> tempArr = new HashSet<>();
         for(int i=0;i<arr.length;i++)
         {
-            if(tempArr.contains(arr[i]))
+            if(!tempArr.add(arr[i]))
                 return true;
-            tempArr.add(arr[i]);
         }
-        
         return false;
     }
 }
