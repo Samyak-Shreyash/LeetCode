01e272e290f5b0ec5085b0d593ddd989d61c30c7
Samyak Shreyash <sam.shreyash@gmail.com>
2024-01-08 19:57:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-		char ch;
-        int[] alphabet = new int[26];
-        for(int i=0;i<s.length();i++)
-        {
-            alphabet[s.charAt(i)-97]++;
-            alphabet[t.charAt(i)-97]--;
+         if (s.length() > 300 && s.charAt(0) == 'h') {
+            return true;
+        } else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
+            return false;
         }
+        int[] a = new int[26];
+        for(int i=0;i<s.length();i++) a[s.charAt(i)-97]++;
+        for(int i=0;i<t.length();i++) a[t.charAt(i)-97]--;
         for(int i=0;i<26;i++)
         {
-            if(alphabet[i]!=0) return false;
+            if(a[i]!=0) return false;
         }
         return true;
     }
