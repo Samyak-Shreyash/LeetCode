2ba8b2afc2cfaf258d7b0fb27263edae6a7be63f
Samyak Shreyash <sam.shreyash@gmail.com>
2024-02-28 09:22:38 +0530

    Time: 7 ms (7.99%), Space: 41.1 MB (28.03%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int c) {
+        String s="";
+        while(c>0)
+        {
+            c--;
+            s=(char)('A'+c%26)+s;
+            c/=26;
+        }
+        return s;
+    }
+}
