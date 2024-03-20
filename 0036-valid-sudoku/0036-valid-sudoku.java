a8d554a2efad88b6fd1f4b45deca0942ddbdaf5d
Samyak Shreyash <sam.shreyash@gmail.com>
2024-03-20 10:53:37 +0530

    Time: 2 ms (78.52%), Space: 44.1 MB (87.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,17 +1,66 @@
 class Solution {
+    public boolean checkRow(char[][] board, int i) {
+        int[] check = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int val = 0;
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] > '0' && board[i][j] <= '9') {
+                val = board[i][j] - '1';
+                if (check[val] != 0) {
+                    return false;
+                }
+                check[val]++;
+            }
+        }
+        return true;
+    }
 
-    public boolean isValidSudoku(char[][] board) {
-        Set soduko = new HashSet();
+    public boolean checkColumn(char[][] board, int j) {
+        int[] check = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int val = 0;
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    String num = "-"+board[i][j];
-                    if (!soduko.add(num + i) || !soduko.add(j+num)
-                            || !soduko.add(i / 3 + num + j / 3))
+
+            if (board[i][j] > '0' && board[i][j] <= '9') {
+                val = board[i][j] - '1';
+                if (check[val] != 0) {
+                    return false;
+                }
+                check[val]++;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkBox(char[][] board, int i, int j) {
+        if (i > 6 || j > 6)
+            return false;
+        int[] check = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int val = 0;
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                if (board[i + k][j + l] > '0' && board[i + k][j + l] <= '9') {
+                    val = board[i + k][j + l] - '1';
+                    if (check[val] != 0) {
                         return false;
+                    }
+                    check[val]++;
                 }
             }
 
+        }
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (!(checkRow(board, i) && checkColumn(board, i)))
+                return false;
+            if (i % 3 == 0) {
+                for (int j = 0; j < 7; j += 3)
+                    if (!checkBox(board, i, j))
+                        return false;
+            }
+            if (!(checkRow(board, i) && checkColumn(board, i)))
+                return false;
         }
         return true;
     }
