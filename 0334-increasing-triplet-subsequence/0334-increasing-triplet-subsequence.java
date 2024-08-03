1126b937c989b11c92f59890c34536a167638ba0
Samyak Shreyash <sam.shreyash@gmail.com>
2024-08-03 12:15:25 +0530

    Time: 3 ms (22.53%), Space: 134.1 MB (15%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,21 +1,13 @@
 class Solution {
-    public boolean increasingOrder(int[] nums, int pos, int rem)
-    {
-        // System.out.print(nums[pos]+" ");
-        if(rem==0)
-            return true;
-        for(int i=pos+1;i<=nums.length-rem;i++)
-        {
-            if(nums[i]>nums[pos])
-                if(increasingOrder(nums,i,rem-1))
-                    return true;
-        }
-        return false;
-    }
     public boolean increasingTriplet(int[] nums) {
-        for(int i=0;i<nums.length-2;i++){
-            if(increasingOrder(nums,i,2))
-                return true;      
+        int a= Integer.MAX_VALUE, b = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<a)
+                a=nums[i];
+            if(nums[i]>a && nums[i]<b)
+                b=nums[i];
+            if(nums[i]>a && nums[i]>b)
+                return true;
         }
         return false;
     }
