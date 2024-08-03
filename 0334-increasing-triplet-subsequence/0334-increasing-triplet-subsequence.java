97a154866b25ab69b033804fed416168ac4714f9
Samyak Shreyash <sam.shreyash@gmail.com>
2024-08-03 12:17:47 +0530

    Time: 3 ms (22.53%), Space: 133.7 MB (64.99%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]<a)
                 a=nums[i];
-            if(nums[i]>a && nums[i]<b)
+            else if(nums[i]>a && nums[i]<b)
                 b=nums[i];
-            if(nums[i]>a && nums[i]>b)
+            else if(nums[i]>a && nums[i]>b)
                 return true;
         }
         return false;
