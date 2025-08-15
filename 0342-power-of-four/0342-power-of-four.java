9da6fb408fff6cb033377406a333a0f4108466f0
Samyak Shreyash <sam.shreyash@gmail.com>
2025-08-15 21:56:45 +0530

    Time: 0 ms (100%), Space: 41.3 MB (15.86%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-       return (n & n - 1) == 0 && (n - 1) % 3 == 0;
-
+        return (n > 0) && ((n & (n - 1)) == 0) && ((n & 0xAAAAAAAA) == 0);
     }
 }
