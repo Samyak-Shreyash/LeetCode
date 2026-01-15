48c9613bfdb609423481677e50f2a0ffb3f7808c
Samyak Shreyash <sam.shreyash@gmail.com>
2026-01-15 13:02:34 +0530

    Time: 301 ms (5.45%), Space: 130.8 MB (80.83%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,26 @@
+class LRUCache {
+    private LinkedHashMap<Integer, Integer> cache;
+    public LRUCache(int capacity) {
+        cache = new LinkedHashMap(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest)
+            {
+                return size()>capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        cache.put(key,value);
+    }
+}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
